d5a2c5088f2bf7e440ffa2864c8831edb0832689
Kai Wang <charles.wangkai@gmail.com>
2015-11-10 18:15:22 +0800

    first commit

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by WangKai on 15/6/10.
+//  Copyright (c) 2015年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
